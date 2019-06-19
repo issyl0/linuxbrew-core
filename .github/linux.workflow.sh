97bf7b0b427e640d0cc245d681545e2aec6283a1
Issy Long <me@issyl0.co.uk>
2019-06-19 21:48:14 +0100

    Add GitHub Actions code for generating formulae data for Linuxbrew
    - This is entirely untested given I don't have access to the Actions
      trial yet.
    - It should push to my fork, when I do.

---
+++ .github/linux.workflow.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -e
+
+# silence bundler complaining about being root
+mkdir ~/.bundle
+echo 'BUNDLE_SILENCE_ROOT_WARNING: "1"' > ~/.bundle/config
+
+# configure git
+git config --global user.name "LinuxbrewTestBot"
+git config --global user.email "homebrew-test-bot@lists.sfconservancy.org"
+
+# create stubs so build dependencies aren't incorrectly flagged as missing
+for i in python svn unzip xz
+do
+  touch /usr/bin/$i
+  chmod +x /usr/bin/$i
+done
+
+CORE_DIR="$(brew --repo linuxbrew/core)"
+mkdir -p "$CORE_DIR"
+rm -rf "$CORE_DIR"
+ln -s "$PWD" "$CORE_DIR"
+
+# Get latest
+git -C "$CORE_DIR" fetch
+git -C "$CORE_DIR" checkout -f master
+git -C "$CORE_DIR" reset --hard origin/master
+
+# setup Homebrew environment
+export HOMEBREW_NO_AUTO_UPDATE=1
+export HOMEBREW_NO_ANALYTICS=1
+export HOMEBREW_FORCE_HOMEBREW_ON_LINUX=1
+export PATH="$(brew --repo)/Library/Homebrew/vendor/portable-ruby/current/bin:$PATH"
+
+# setup SSH
+mkdir ~/.ssh
+chmod 700 ~/.ssh
+echo "$FORMULAE_DEPLOY_KEY" > ~/.ssh/id_ed25519
+chmod 600 ~/.ssh/id_ed25519
+git config --global core.sshCommand "ssh -i ~/.ssh/id_ed25519 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+
+# clone formulae.brew.sh with SSH so we can push back
+git clone git@github.com:issyl0/formulae.brew.sh
+cd formulae.brew.sh
+
+# setup analytics
+echo "$ANALYTICS_JSON_KEY" > ~/.homebrew_analytics.json
+unset HOMEBREW_NO_ANALYTICS
+
+# run rake (without a rake binary)
+ruby -e "load Gem.bin_path('rake', 'rake')"
+
+# commit and push generated files
+git add _data/formula-linux api/formula-linux formula-linux
+git diff --exit-code HEAD -- _data/analytics-linux _data/formula-linux api/formula-linux formula-linux && exit 0
+git commit -m 'formula: update from Homebrew/linuxbrew-core push' _data/analytics-linux _data/formula-linux api/formula-linux formula-linux
+git push
