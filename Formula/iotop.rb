e4e810745fb964df0a3e9f8136e70decf9aaf93b
Issy Long <me@issyl0.co.uk>
2019-08-26 23:22:09 +0100

    iotop: wip

--- Formula/iotop.rb
+++ Formula/iotop.rb
@@ -3,8 +3,8 @@ class Iotop < Formula
   homepage "http://guichaz.free.fr/iotop/"
   url "http://guichaz.free.fr/iotop/files/iotop-0.6.tar.bz2"
   sha256 "3adea2a24eda49bbbaeb4e6ed2042355b441dbd7161e883067a02bfc8dcef75b"
+  revision 1
   head "git://repo.or.cz/iotop.git"
-  # tag "linuxbrew"
 
   bottle do
     cellar :any_skip_relocation
@@ -13,11 +13,11 @@ class Iotop < Formula
   depends_on "python@2"
 
   def install
-    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
     system "python", *Language::Python.setup_install_args(libexec)
 
-    bin.install Dir[libexec/"bin/*"]
-    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"], :PATH => "#{libexec}/bin:$PATH")
+    bin.install Dir[libexec/"sbin/*"]
+    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
   end
 
   test do
