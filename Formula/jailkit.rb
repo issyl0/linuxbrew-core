dff81ac1c4b7677f75f59638343d3b30c3414122
Issy Long <me@issyl0.co.uk>
2020-02-19 19:24:44 +0000

    jailkit: Python 2 => Python 3.8

--- Formula/jailkit.rb
+++ Formula/jailkit.rb
@@ -8,13 +8,13 @@ class Jailkit < Formula
     sha256 "b0980932e53c18886c0106fd50a6101fc77754fcfb536bf218fd6845f7e91309" => :catalina
     sha256 "1aae3a9499ec0553b45b48003e43c55e1facd7b56d94cc4ff9cb639496b7fa05" => :mojave
     sha256 "25e55b2cc8572ad043c97ad2b86e08f93ef1a69c6fb66d1bf1630c4c5092bdfc" => :high_sierra
-    sha256 "374a530174e1612ba2fd7b0ac512599a625876c6633b6b3b7fb9d9b0e23cea4a" => :x86_64_linux
   end
 
-  depends_on :macos # Due to Python 2
-  uses_from_macos "python@2"
+  depends_on "python@3.8"
 
   def install
+    ENV["PYTHONINTERPRETER"] = Formula["python@3.8"].opt_bin/"python3"
+
     system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
     system "make", "install"
   end
